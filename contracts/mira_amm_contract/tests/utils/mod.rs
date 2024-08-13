76502db32f1eaa148e9ed642011029aa67dfb7c1
Sarmat <skhachirov12@gmail.com>
2024-08-13 13:52:47 +0300

    feat: test harness & `create_pool` tests

---
+++ contracts/mira_amm_contract/tests/utils/mod.rs
@@ -0,0 +1,51 @@
+use fuels::{
+    accounts::wallet::WalletUnlocked,
+    types::{AssetId, Bits256, ContractId},
+};
+use test_harness::{
+    data_structures::WalletAssetConfiguration,
+    interface::{
+        mock::{add_token, deploy_mock_token_contract, get_sub_id},
+        MiraAMM, MockToken,
+    },
+    setup::common::{deploy_amm, setup_wallet_and_provider},
+    utils::common::{order_sub_ids, order_token_ids},
+};
+
+pub async fn setup() -> (
+    MiraAMM<WalletUnlocked>,
+    WalletUnlocked,
+    ContractId,
+    MockToken<WalletUnlocked>,
+    (AssetId, AssetId),
+    (Bits256, Bits256),
+) {
+    let (wallet, _asset_ids, _provider) =
+        setup_wallet_and_provider(&WalletAssetConfiguration::default()).await;
+    let amm = deploy_amm(&wallet).await;
+    let (token_contract_id, token_contract) = deploy_mock_token_contract(&wallet).await;
+
+    // setup two asset pairs that will be used in tests
+    // let asset_pairs = vec![(asset_ids[0], asset_ids[1]), (asset_ids[1], asset_ids[2])];
+
+    let token_a_id =
+        add_token(&token_contract, "TOKEN_A".to_string(), "TKA".to_string(), 9).await.value;
+    let token_b_id =
+        add_token(&token_contract, "TOKEN_B".to_string(), "TKB".to_string(), 9).await.value;
+
+    let token_a_sub_id = get_sub_id(&token_contract, token_a_id).await.value.unwrap();
+    let token_b_sub_id = get_sub_id(&token_contract, token_b_id).await.value.unwrap();
+
+    let (token_a_sub_id, token_b_sub_id) =
+        order_sub_ids((token_a_id, token_b_id), (token_a_sub_id, token_b_sub_id));
+    let (token_a_id, token_b_id) = order_token_ids((token_a_id, token_b_id));
+
+    (
+        amm.instance,
+        wallet,
+        token_contract_id,
+        token_contract,
+        (token_a_id, token_b_id),
+        (token_a_sub_id, token_b_sub_id),
+    )
+}
---
+++ test-harness/src/paths.rs
@@ -0,0 +1,8 @@
+pub const AMM_CONTRACT_BINARY_PATH: &str =
+    "../../contracts/mira_amm_contract/out/debug/mira_amm_contract.bin";
+pub const AMM_STORAGE_PATH: &str =
+    "../../contracts/mira_amm_contract/out/debug/mira_amm_contract-storage_slots.json";
+
+// Mocks
+pub const MOCK_TOKEN_CONTRACT_BINARY_PATH: &str =
+    "../../contracts/mocks/mock_token/out/debug/mock_token.bin";
